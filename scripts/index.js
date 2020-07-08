30349d78d68d3928200bd4c831616fcf54cb3022
trunov <k.trunov.k@yandex.ru>
2020-07-08 22:54:25 +0300

    feat: add popup js functionality; fix: refactor index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,31 @@
-const editButton = document.querySelector('.profile__edit-button');
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector('.profile__edit-button');
+const profileName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
+
 const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__close-button');
+const closeButton = popup.querySelector('.popup__close-button');
+const text = popup.querySelector('.popup__text_name');
+const description = popup.querySelector('.popup__text_description');
+const popupForm = popup.querySelector('.popup__form');
 
-
-function openPopUp() {
+function toggle() {
     popup.classList.toggle('popup__open');
 }
 
+function openPopUp() {
+    text.value = profileName.textContent;
+    description.value = profileDescription.textContent;
+    toggle();
+}
+
+function submitForm(evt) {
+    evt.preventDefault();
+    profileName.textContent = text.value;
+    profileDescription.textContent = description.value;
+    toggle();
+}
+
 editButton.addEventListener('click', openPopUp);
-closeButton.addEventListener('click', openPopUp);
+closeButton.addEventListener('click', toggle);
+popupForm.addEventListener('submit', submitForm);
