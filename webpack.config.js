f33691c4c06a836458a0cfda8b0239bd15f5a6c1
trunov <k.trunov.k@yandex.ru>
2020-09-08 14:28:25 +0300

    feat: add css/file loader and miniCss plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -18,8 +20,32 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff2|woff|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
       }
     ]
-  }
-  
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: './src/images/favicon.ico',
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ] 
+
 };
